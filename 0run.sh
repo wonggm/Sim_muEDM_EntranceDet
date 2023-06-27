a90f67f67887f6b87982d2b7e51638f51521350b
wonggm <134043648+wonggm@users.noreply.github.com>
2023-06-27 11:05:48 +0800

    Update 0run.sh

--- 0run.sh
+++ 0run.sh
@@ -8,3 +8,5 @@ source /cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos7-gcc11-opt/setup.sh
 
 export PATH="$PATH:/lustre/collider/wonggm/musrSim_Twiss/build"
 echo "musrSim path: " $(which musrSim)
+
+musrSim 100masterWithAPC.mac
